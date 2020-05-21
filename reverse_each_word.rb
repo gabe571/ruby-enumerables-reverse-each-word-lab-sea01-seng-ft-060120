d1739ef072b4532d6b6489f298bf637cb9d167f5
Gabriel Castro <gabriel.castro1028@gmail.com>
2020-05-21 04:22:40 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,11 @@
+
+def reverse_each_word
+text = "Hello there, and how are you?"
+puts text: #{text}"
+
+wordsReversed = text.split(" ").reverse.join(" ")
+
+puts "Words reveresed #{wordsReversed}"
+end
+end 
+
